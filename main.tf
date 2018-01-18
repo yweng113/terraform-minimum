ae2176d9a5fe3cd39a3ffcb7dd4c551029aa2195
Nick Fagerlund <nick.fagerlund@gmail.com>
2018-01-17 16:05:39 -0800

    Let's try this.

--- main.tf
+++ main.tf
@@ -1,6 +1,19 @@
 variable "username" {}
+variable "prior_workspace" {
+  default = ""
+}
 
 provider "random" {} # an empty for destroying old resources
+
+data "terraform_remote_state" "prior_workspace" {
+  backend = "atlas"
+  config {
+    name = "${var.prior_workspace}"
+  }
+  defaults {
+    username = "default"
+  }
+}
 
 resource "null_resource" "random" {
   triggers = {
@@ -15,4 +28,8 @@ output "random" {
 
 output "username" {
   value = "Username is ${var.username}. Extra text."
+}
+
+output "prior_username" {
+  value = "Prior username: ${data.terraform_remote_state.prior_workspace.username}"
 }
