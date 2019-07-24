de15ec5410ed9229a4a739d676693edc36b3797b
Nick Fagerlund <nick.fagerlund@gmail.com>
2019-07-23 17:19:09 -0700

    Make child use a module from otherchild

--- otherchild/main.tf
+++ otherchild/main.tf
@@ -1,10 +1,5 @@
 resource "null_resource" "random" { # reusing the name just to confirm some unrelated behavior
   triggers = {
-    username = "hardcoder"
+    username = "aoeuaeoa"
   }
 }
-
-module "otherchild" {
-  source = "../otherchild"
-}
-
