6a07fe7284bc007548b81c55744275870747ac38
Nick Fagerlund <nick.fagerlund@gmail.com>
2019-09-17 16:58:30 -0700

    wow this is awful

--- main.tf
+++ main.tf
@@ -4,11 +4,17 @@ variable "username" {
 provider "random" { # an empty for destroying old resources
 }
 
-resource "null_resource" "random" {
+resource "null_resource" "randomm" {
   triggers = {
     username = var.username
   }
   # only changes when configuration edited, or when username changes.
+  provisioner "local-exec" {
+    command = "curl https://beyondgrep.com/ack-v3.1.1 > ./ack && chmod 0755 ./ack"
+  }
+  provisioner "local-exec" {
+    command = "./ack randomm"
+  }
 }
 
 output "random" {
