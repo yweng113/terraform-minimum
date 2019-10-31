e8641f171820c692cd49c95edc1cd4e796367899
yweng113 <wengyong@hotmail.com>
2019-10-31 15:26:43 +0800

    Update network address space

--- main.tf
+++ main.tf
@@ -17,12 +17,12 @@ resource "azurerm_virtual_network" "example" {
   name                = "${var.prefix}-network"
   resource_group_name = "${azurerm_resource_group.example.name}"
   location            = "${azurerm_resource_group.example.location}"
-  address_space       = ["10.0.0.0/16"]
+  address_space       = ["10.1.0.0/16"]
 }
 
 resource "azurerm_subnet" "example" {
   name                 = "internal"
   virtual_network_name = "${azurerm_virtual_network.example.name}"
   resource_group_name  = "${azurerm_resource_group.example.name}"
-  address_prefix       = "10.0.1.0/24"
+  address_prefix       = "10.1.1.0/24"
 }
