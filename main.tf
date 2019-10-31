5852d755cfc7221d43ff5d412a447a341f6ae702
yweng113 <wengyong@hotmail.com>
2019-10-31 15:09:29 +0800

    Update the demo project
    Add variables.tf and update main.tf

--- main.tf
+++ main.tf
@@ -1,45 +1,28 @@
-variable "username" {
+provider "azurerm" {
+  # Whilst version is optional, we /strongly recommend/ using it to pin the version of the Provider being used
+  version = "=1.36.0"
+
+  subscription_id = "2a579ce9-e787-4a87-bc31-da8a7ac529f6"
+  client_id       = "72e0d2ef-b084-49d2-97a5-d5c566d4f2ae"
+  client_secret   = "${var.client_secret}"
+  tenant_id       = "2998bf45-9979-4820-aa1e-0e631462e528"
 }
 
-provider "random" { # an empty for destroying old resources
+resource "azurerm_resource_group" "example" {
+  name     = "${var.prefix}-resources"
+  location = "${var.location}"
 }
 
-resource "null_resource" "randomm" {
-  triggers = {
-    username = var.username
-  }
-  # only changes when configuration edited, or when username changes.
-  provisioner "local-exec" {
-    command = "curl https://beyondgrep.com/ack-v3.1.1 > ./ack && chmod 0755 ./ack"
-  }
-  provisioner "local-exec" {
-    command = "./ack randomm"
-  }
+resource "azurerm_virtual_network" "example" {
+  name                = "${var.prefix}-network"
+  resource_group_name = "${azurerm_resource_group.example.name}"
+  location            = "${azurerm_resource_group.example.location}"
+  address_space       = ["10.0.0.0/16"]
 }
 
-output "random" {
-  value = "Changed to: ${null_resource.randomm.id}"
-}
-
-output "username" {
-  value = "Username is ${var.username}. Extra text."
-}
-
-data "terraform_remote_state" "dev" {
-  backend = "remote"
-  workspace = "terraform-minimum"
-  config = {
-    organization = "ywengdemo"
-    workspaces = {
-      name = "terraform-minimum"      
-    }
-  }
-}
-
-output "username-dev" {
-  value = data.terraform_remote_state.dev.outputs.username
-}
-
-output "all-dev" {
-  value = data.terraform_remote_state.dev.outputs
+resource "azurerm_subnet" "example" {
+  name                 = "internal"
+  virtual_network_name = "${azurerm_virtual_network.example.name}"
+  resource_group_name  = "${azurerm_resource_group.example.name}"
+  address_prefix       = "10.0.1.0/24"
 }
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "prefix" {
+  description = "The prefix used for all resources in this example"
+}
+
+variable "location" {
+  description = "The Azure location where all resources in this example should be created"
+}
+
+variable "client_secret" {}
